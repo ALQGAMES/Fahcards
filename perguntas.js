a0da0c81b9d4138764b836807954beed6ddae3a2
André Loss Q <106595184+ALQGAMES@users.noreply.github.com>
2024-11-27 10:23:55 -0300

    correção

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Segunda Guerra',
+    'COMEÇOU EM 1939 E ACABOU EM 1945?',
+    'SEGUNDA GUERRA MUNDIAL'
+)
+
+criaCartao(
+    'Segunda Guerra',
+    'QUAL FOI O MAIOR ATAQUE DO JAPÃO AO ESTADOE UNIDOS?',
+    'PEARL HARBOUR'
+)
+
+criaCartao(
+    'Segunda Guerra',
+    'QUAL O ARMAMENTO BANIDO APÓS A SEGUNDA GUERRA POR SER CONSIDERADA "DESHUMANA"',
+    'GÁS MOSTARDA'
+)
